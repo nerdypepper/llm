a7a43356874b0e2296f8c332cef13e6dab1af1fa
Akshay <nerdy@peppe.rs>
2023-08-03 12:22:09 +0530

    bulk up bench script

--- crates/llm/examples/bench.rs
+++ crates/llm/examples/bench.rs
@@ -26,14 +26,50 @@ impl Args {
     }
 }
 
+#[derive(Clone)]
+struct BenchResult {
+    elapsed: std::time::Duration,
+    query_token_count: usize,
+}
+
+impl BenchResult {
+    /// number of tokens embedded per millisecond
+    fn rate(&self) -> f64 {
+        (self.query_token_count as f64) / (self.elapsed.as_millis() as f64)
+    }
+}
+
+impl std::fmt::Display for BenchResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} tokens, {} ms",
+            self.query_token_count,
+            self.elapsed.as_millis(),
+        )
+    }
+}
+
+impl std::ops::Add<BenchResult> for BenchResult {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            elapsed: self.elapsed.add(rhs.elapsed),
+            query_token_count: self.query_token_count + rhs.query_token_count,
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
     let tokenizer_source = args.to_tokenizer_source();
     let model_architecture = args.model_architecture;
     let model_path = args.model_path;
-    let query = &include_str!("./inference.rs")[..400];
-    // let query = include_str!("./inference.rs");
+    let corpus = &include_str!("./vicuna-chat.rs")
+        .lines()
+        .map(|l| &l[..500.min(l.len())])
+        .collect::<Vec<_>>();
 
     // Load model
     let mut model_params = llm::ModelParameters::default();
@@ -54,15 +90,38 @@ fn main() {
     let inference_parameters = llm::InferenceParameters::default();
 
     // Generate embeddings for query and comparands
-    let s = std::time::Instant::now();
-    let query_embeddings = get_embeddings(model.as_ref(), &inference_parameters, query);
+    let mut results = corpus
+        .into_iter()
+        .map(|l| get_embeddings(model.as_ref(), &inference_parameters, l))
+        .collect::<Vec<_>>();
+    results.sort_by(|a, b| {
+        a.elapsed
+            .cmp(&b.elapsed)
+            .then(a.query_token_count.cmp(&b.query_token_count))
+    });
+
+    let fastest = results.first().unwrap();
+    let slowest = results.last().unwrap();
+
+    println!("slowest: {:.04} tok/ms ({})", slowest.rate(), slowest);
+    println!("fastest: {:.04} tok/ms ({})", fastest.rate(), fastest);
+    println!(
+        "average: {:.04} tok/ms, over {} readings",
+        results
+            .clone()
+            .into_iter()
+            .reduce(|acc, x| acc + x)
+            .unwrap()
+            .rate(),
+        results.len(),
+    );
 }
 
 fn get_embeddings(
     model: &dyn llm::Model,
-    inference_parameters: &llm::InferenceParameters,
+    _inference_parameters: &llm::InferenceParameters,
     query: &str,
-) -> Vec<f32> {
+) -> BenchResult {
     let s = std::time::Instant::now();
     let mut session = model.start_session(Default::default());
     let mut output_request = llm::OutputRequest {
@@ -77,15 +136,10 @@ fn get_embeddings(
         .iter()
         .map(|(_, tok)| *tok)
         .collect::<Vec<_>>();
-    println!("input length: {}", query_token_ids.len());
     model.evaluate(&mut session, &query_token_ids, &mut output_request);
-    let embeddings = output_request.embeddings.unwrap();
-    let t = s.elapsed().as_millis();
-    let l = query_token_ids.len();
-    let l_over_t = l as f32 / t as f32;
-    println!(
-        "input len:{}, time elapsed: {}ms, len/ms: {}",
-        l, t, l_over_t
-    );
-    embeddings
+    let _embeddings = output_request.embeddings.unwrap();
+    BenchResult {
+        elapsed: s.elapsed(),
+        query_token_count: query_token_ids.len(),
+    }
 }
